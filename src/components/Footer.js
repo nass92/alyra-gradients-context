7de0d613510b4f46cde035eed9c9114cabd4e8ee
Westindiess <gregoryalexiss@gmail.com>
2021-05-30 22:48:56 +0200

    Project-gradient-T-white

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,12 @@
 const Footer = () => {
 	return (
-		<>
-			<footer className="text-center text-white bg-dark p-3 mt-auto">
-				Made with <span className="text-danger">&hearts;</span>
-				<span className="text-warning">&hearts;</span>
-				<span className="text-primary">&hearts;</span>
-				<span className="text-success">&hearts;</span> & React by Nassim,
-				Grégory, Victor & Sylvie for Alyra White Team
-			</footer>
-		</>
+		<footer className="text-center text-white bg-dark p-3 mt-auto">
+			Made with <span className="text-danger">&hearts;</span>
+			<span className="text-warning">&hearts;</span>
+			<span className="text-primary">&hearts;</span>
+			<span className="text-success">&hearts;</span> & React by Nassim, Grégory,
+			Victor & Sylvie for Alyra White Team
+		</footer>
 	)
 }
 
